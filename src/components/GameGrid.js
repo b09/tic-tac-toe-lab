b9dccdca83df990f30ce9b1288658b007713d726
Simon Atkins <this-is-simon@github.com>
2018-06-13 11:00:47 +0100

    Set up initial files

---
+++ src/components/GameGrid.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import GameCell from './GameCell.js';
+
+
+// export default GameGrid;
---
+++ src/containers/GameContainer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import GameGrid from '../components/GameGrid.js';
+// import GameCell from '../components/GameCell.js';
+
+class GameContainer extends React.Component {
+
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return(
+      <div>
+        Hello World...
+      </div>
+    )
+  }
+
+
+}
+
+
+
+export default GameContainer;
